5792bd65e4de6f159b10e69d095dc5233b374093
Sanjana <sanjanakallu2013@gmail.com>
2025-10-28 18:12:12 +0530

    Changed base to repo name and redeployed

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { copyFileSync } from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    {
+    name: "copy-404",
+      closeBundle() {
+        try {
+          copyFileSync("dist/index.html", "dist/404.html");
+          console.log("✅ 404.html copied for GitHub Pages SPA routing");
+        } catch (e) {
+          console.error("❌ Failed to copy 404.html:", e);
+        }
+      },
+    },
+  ],
+  base: "/portfolio/",
   optimizeDeps: {
   include: ["react-responsive-carousel"],
 }
